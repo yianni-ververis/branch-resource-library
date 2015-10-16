4c1a711706fc52877ee323e3e9b195428f9fc02d
Nick Webster <nick.webster@qlikview.com>
2015-10-16 11:06:27 +0100

    Captcha mechanism implemented on signup form

--- public/scripts/build/app.js
+++ public/scripts/build/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module("branch", ["ui.router", "ngResource", "ngConfirm", "ngNotifications", "ngComments", "ngModeration", "ngSanitize", 'ui.bootstrap' ]);
+  var app = angular.module("branch", ["ui.router", "ngResource", "ngConfirm", "ngNotifications", "ngComments", "ngModeration", "ngSanitize", 'ui.bootstrap', "visualCaptcha" ]);
 
   app.config(["$stateProvider","$urlRouterProvider", "confirmConfigProvider", "notificationConfigProvider", "commentsConfigProvider", "moderationConfigProvider", function($stateProvider, $urlRouterProvider, notificationsConfigProvider, confirmConfigProvider, commentsConfig, moderationConfig) {
     $urlRouterProvider.otherwise("/");
@@ -1600,11 +1600,24 @@
   app.controller("authController", ["$scope", "$resource", "$state", "$stateParams", "userManager", "resultHandler", "notifications", function($scope, $resource, $state, $stateParams, userManager, resultHandler, notifications){
     var Login = $resource("auth/login");
     var Signup = $resource("auth/signup");
-    var Reset = $resource("auth/reset")
+    var Reset = $resource("auth/reset");
+    var Captcha = $resource("visualcaptcha/try");
 
     if($stateParams.url){
       $scope.returnUrl = $stateParams.url.replace(/%2F/gi, '');
     }
+
+    $scope.captchaOptions = {
+                  imgPath: 'bower_components/visualcaptcha.angular/img/',
+                  captcha: {
+                      numberOfImages: 4,
+                      url: "/visualcaptcha",
+                      supportsAudio: false
+                  },
+                  init: function ( captcha ) {
+                      $scope.captcha = captcha;
+                  }
+              };
 
     $scope.login = function(){
       Login.save({
@@ -1622,19 +1635,34 @@
     };
 
     $scope.signup = function(){
-      Signup.save({
-        username: $scope.username,
-        password: $scope.password,
-        email: $scope.email
-      }, function(result) {
-        if(resultHandler.process(result)){
-          userManager.refresh();
-          window.location = "#" + $scope.returnUrl || "/";
-        }
-        else{
-          notifications.notify(result.errText, null, {sentiment: 'negative'});
-        }
-      })
+      //check the captcha form has been filled
+      if ($scope.isVisualCaptchaFilled()){
+        //check that the captcha selection is correct
+        var captchaField = $(".imageField")[0];
+        var captchaCheck = {};
+        captchaCheck[$(captchaField).attr('name')] = $(captchaField).attr('value');
+        console.log(captchaCheck);
+        Captcha.save(captchaCheck, function(result){
+          if(result.status=="valid"){
+            Signup.save({
+              username: $scope.username,
+              password: $scope.password,
+              email: $scope.email
+            }, function(result) {
+              if(resultHandler.process(result)){
+                userManager.refresh();
+                window.location = "#" + $scope.returnUrl || "/";
+              }
+              else{
+                notifications.notify(result.errText, null, {sentiment: 'negative'});
+              }
+            });
+          }
+          else{
+            notifications.notify("Specified captcha is not correct", null, {sentiment: 'negative'});
+          }
+        });
+      }
     };
 
     $scope.reset = function() {
@@ -1649,6 +1677,14 @@
           notifications.notify(result.errText, null, {sentiment: 'negative'});
         }
       })
+    };
+
+    $scope.isVisualCaptchaFilled = function() {
+        if ( !$scope.captcha.getCaptchaData().valid ) {
+            notifications.notify("visualCaptcha is NOT filled", null, {sentiment: 'warning'});
+            return false;
+        }
+        return true;
     };
 
   }]);
