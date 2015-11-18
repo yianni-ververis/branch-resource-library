afa74f1689fe494610437d53a99a608f43fd2516
Nick Webster <nick.webster@qlikview.com>
2015-11-18 19:37:38 +0100

    changed notification email to DL-PartnerEngineering

--- server/controllers/mailText.js
+++ server/controllers/mailText.js
@@ -1,38 +1,39 @@
 module.exports = {
   signup:{
     user:{
-      to: "nwr@qlik.com",
+      to: "DL-PartnerEngineering",
       subject: "Welcome to Branch",
       html: "{{username}}, thank you for joining the Branch community."
     }
   },
   create:{
     comment:{
-      to: "nwr@qlik.com",
+      to: "DL-PartnerEngineering",
       subject: "New Activity on {{parent.title}}",
       html: "{{comment.content}}"
     }
   },
   unapprove:{
     userprofile:{
-      to: "nwr@qlik.com",
+      to: "DL-PartnerEngineering",
       subject: "Branch User Blocked",
       html: "The user x on branch.qlik.com has been blocked blah blah blah"
     },
     project:{
-      to: "nwr@qlik.com",
+      to: "DL-PartnerEngineering",
       subject: "Branch Project Rejected",
-      html: "Project {{title}} on branch.qlik.com has been rejected with the following comments.<br /><p>{{hide_comment}}</p>"
+      html: "Please do not be alarmed, this is a test. You're project hasn't really been rejected"
+      //html: "Project {{title}} on branch.qlik.com has been rejected with the following comments.<br /><p>{{hide_comment}}</p>"
     }
   },
   update:{
     subscription:{
-      to: "nwr@qlik.com",
+      to: "DL-PartnerEngineering",
       subject: "{{title}} has been updated",
       html: "Some relevant text here"
     },
     subscriptionComment:{
-      to: "nwr@qlik.com",
+      to: "DL-PartnerEngineering",
       subject: "Someone has commented on {{parent.title}}",
       html: "{{comment.content}}"
     }
