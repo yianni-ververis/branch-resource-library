9a8819cd0074fdae235a1cf5ec62cdd7ac000aa5
mindspank <alec.karlsson@gmail.com>
2015-11-20 13:33:00 -0500

    moving more stuff into config

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  host: "qtdevrelations:3001"
+  host: "qtdevrelations:3001",
+  qrs: {
+    host: "10.211.55.3",
+    app: "bf6c1ed8-69fb-4378-86c2-a1c71a2b3cc1"
+  },
+  mongoconnectionstring: "mongodb://qtdevrelations:27017/branch"
 }
