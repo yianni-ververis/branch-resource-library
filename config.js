3b5d2c0ce4ec7f99eb3fabf54be78e46ecc4ddfe
Nick Webster <nick.webster@qlikview.com>
2015-11-24 16:57:09 +0000

    notification changes

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
     host: "10.211.55.3",
     app: this.host === "qtdevrelations" ? "bf6c1ed8-69fb-4378-86c2-a1c71a2b3cc1" : "NICK UPDATE WITH APP ID FROM BRANCH-TEST"
   },
-  mongoconnectionstring: "mongodb://qtdevrelations:27017/branch"
+  mongoconnectionstring: "mongodb://branch-test.qlik.com:27017/branch"
 }
