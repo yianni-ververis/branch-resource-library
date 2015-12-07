66de5c6429f84cefa6c51fcad52fab4a7e7910d6
mindspank <alec.karlsson@gmail.com>
2015-12-07 16:54:57 -0500

    massive commit because I'm lazy

--- public/scripts/raw/directives/subscribe.js
+++ public/scripts/raw/directives/subscribe.js
@@ -34,7 +34,7 @@
 				user: "="
 			},
       template: function(elem, attr){
-        var html = "<button class='branch-button' ng-show='user' ng-click='toggleSubscription()' ng-disabled='{{subscribing==true}}'>{{buttonText}}</button>";
+        var html = "<button class='branch-light-button' ng-show='user' ng-click='toggleSubscription()' ng-disabled='{{subscribing==true}}'><i class='fa fa-envelope-o'></i>{{buttonText}}</button>";
         return html;
       },
       link: function(scope){
