1d17266d3bd1ddfb307f21ad01ae23b53ad2a9e8
Nick Webster <nick.webster@qlikview.com>
2016-07-06 16:12:10 +0200

    added domain option to session cookie (i hope)

--- server.js
+++ server.js
@@ -67,7 +67,11 @@ app.use('/attachments', express.static(__dirname + '/public/attachments'));
 app.use("/qsocks", express.static(__dirname + "/node_modules/qsocks"));
 app.use("/configs", express.static(__dirname + "/public/configs"));
 
-app.use(expressSession({secret: 'mySecretKey', store: new MongoStore({ mongooseConnection: mongoose.connection})}));
+app.use(expressSession({
+  secret: 'mySecretKey',
+  store: new MongoStore({ mongooseConnection: mongoose.connection}),
+  cookie: {path:"/", domain:config.cookieDomain, httpOnly: true}
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 
